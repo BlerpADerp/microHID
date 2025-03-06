6dd1669ff43dc1154d99e6af64f208d098520e9f
BlerpADerp <sebsidedown@gmail.com>
2025-03-06 16:41:31 -0500

    Create Plasma.py

---
+++ Plasma.py
@@ -0,0 +1,28 @@
+{
+	"format_version": "1.16.0",
+	"minecraft:geometry": [
+		{
+			"description": {
+				"identifier": "geometry.plasma",
+				"texture_width": 32,
+				"texture_height": 32,
+				"visible_bounds_width": 2,
+				"visible_bounds_height": 1.5,
+				"visible_bounds_offset": [0, 0.25, 0]
+			},
+			"bones": [
+				{
+					"name": "root_item",
+					"pivot": [0, 0, 0],
+					"binding": "q.item_slot_to_bone_name(c.item_slot)",
+					"cubes": [
+						{"origin": [-4.5, 0, 0.5], "size": [5, 5, 5], "uv": [0, 0]},
+						{"origin": [-5.4, 4, -1.7], "size": [6.8, 2, 7], "pivot": [0, 2, -1.2], "rotation": [-37.5, 0, 0], "uv": [-2, 20]},
+						{"origin": [-5, 2, -0.2], "size": [5.9, 2, 6.1], "pivot": [0, 0, -0.2], "rotation": [-2.5, 0, 0], "uv": [0, 22]},
+						{"origin": [-5.5, 9, 2.4], "size": [7, 2, 7], "pivot": [0, 7, 2.9], "rotation": [-137.5, 0, 0], "uv": [-3, 20]}
+					]
+				}
+			]
+		}
+	]
+}
